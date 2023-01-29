40dd68d4ee74512236771b531ecc19d0055b091d
Джабраил Курданов <dzabrailkurdanov@MacBook-Air-Dzabrail.local>
2023-01-29 11:17:50 +0300

    feat: Initial commit

---
+++ robots/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Robot
+
+class RobotAdmin(admin.ModelAdmin):
+    list_display = ['serial', 'model', 'version', 'created']
+    list_filter = ['created']
+
+admin.site.register(Robot, RobotAdmin)
---
+++ robots/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from django.core.exceptions import ValidationError
+
+
+class Robot(models.Model):
+    serial = models.CharField(max_length=5, blank=False, null=False)
+    model = models.CharField(max_length=2, blank=False, null=False)
+    version = models.CharField(max_length=2, blank=False, null=False)
+    created = models.DateTimeField(blank=False, null=False)
